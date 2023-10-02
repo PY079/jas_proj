fc268d6019a161bab538ec96ade61559c88b976a
Ratata <n.cowalyow2017@yandex.ru>
2023-10-02 13:51:10 +0300

    upd js

--- default_param.js
+++ default_param.js
@@ -2,8 +2,8 @@ const defaultB = 30
 const getDefaul = c => c **2
 
 
-function computer(a,b=getDefaul(a)) {
+function compute(a,b=getDefaul(a)) {
     return a+b
 }
 
-console.log(computer(20))
+console.log(compute(20))
--- func.js
+++ func.js
@@ -2,25 +2,25 @@ let data = ''
 function sum(a,b) { 
     console.log('a:',a,'b:',b)
     console.log('Сумма a,b:',a+b)
-    // document.writeln('a: ',a,'b: ',b)
-    // document.writeln('Сумма a,b: ',a+b)
+    document.writeln('a: ',a,'b: ',b)
+    document.writeln('Сумма a,b: ',a+b)
 
     
     // return a+b
 }
-// sum(-1,2)
+sum(-1,2)
 
-function log() {
-    console.log(this)
-}
+// function log() {
+//     console.log(this)
+// }
 
-const errLog = () => console.log(this)
+// const errLog = () => console.log(this)
 
-const person = {
-    name: 'Elena',
-    age: 20,
-    log: log,
-    errLog: errLog
-}
+// const person = {
+//     name: 'Elena',
+//     age: 20,
+//     log: log,
+//     errLog: errLog
+// }
 
-person.log()
+// person.log()
